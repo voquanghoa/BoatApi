c4db85b165fd972be0108da058eef104207b516b
Võ Quang Hòa <voquanghoa@gmail.com>
2016-03-18 18:40:13 +0700

    Add files

---
+++ BoatApi/Business/AuthenticationBusiness.cs
@@ -0,0 +1,64 @@
+﻿using BoatApi.Business.Logic;
+using BoatApi.Business.Logic.Common;
+using BoatApi.Models.Communication.Request;
+using BoatApi.Models.ServiceModel;
+using BoatApi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace BoatApi.Business
+{
+	public class AuthenticationBusiness
+	{
+		private const string SessonHashCookieName = "SESSONHASH";
+
+		private readonly UserRepository userRepository;
+		private readonly BaseRepository<Authentication> authenticationRepository;
+
+		public AuthenticationBusiness(IUnitOfWork unitOfWork)
+		{
+			authenticationRepository = new BaseRepository<Authentication>(unitOfWork);
+			userRepository = new UserRepository(unitOfWork);
+		}
+
+		public bool Authenticate(LoginForm loginForm)
+		{
+			var user = userRepository.FindUser(loginForm.Email, loginForm.Password);
+
+			if (user == null)
+			{
+				return false;
+			}
+
+			var authentication = new Authentication()
+			{
+				Id = Guid.NewGuid(),
+				SessionHash = Guid.NewGuid(),
+				User = user
+			};
+
+			CookieUtil.SetCookie(SessonHashCookieName, authentication.SessionHash.ToString());
+			authenticationRepository.Create(authentication);
+
+			return true;
+		}
+
+		public bool VerifyAuthenticated()
+		{
+			var sessonHashCookie = CookieUtil.GetCookie(SessonHashCookieName);
+			if (sessonHashCookie != null)
+			{
+				var sessonGuid = Guid.Parse(sessonHashCookie);
+				if (authenticationRepository.Contains(x => x.SessionHash == sessonGuid))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ BoatApi/Business/Logic/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using BoatApi.Business.Logic.Common;
+using BoatApi.Models.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoatApi.Business.Logic
+{
+	public class UserRepository: BaseRepository<User>
+	{
+		public UserRepository(IUnitOfWork unitOfWork):base(unitOfWork)
+		{
+		}
+
+		public User FindUser(string email, string password)
+		{
+			return FindOne(x => x.Email == email && x.Password == password);
+		}
+	}
+}
